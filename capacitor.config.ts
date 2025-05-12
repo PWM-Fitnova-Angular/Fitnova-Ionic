eed5e0e2a58d279af89252eb01a0a0d00f627a1b
Dani <danielml2003.dml@gmail.com>
2025-05-12 17:37:33 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fitnova',
+  webDir: 'www'
+};
+
+export default config;
