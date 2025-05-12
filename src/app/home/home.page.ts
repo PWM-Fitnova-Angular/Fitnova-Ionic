e143a71d0db1da3d3d6978f50349df035b7f0d5e
Dani <danielml2003.dml@gmail.com>
2025-05-12 18:04:27 +0100

    Init proyect

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  imports: [
+    IonicModule,
+    FormsModule
+  ]
 })
 export class HomePage {
-  constructor() {}
+  name = '';
+  email = '';
+  phoneNumber = '';
+  birthday = '';
+
+  Registrarse() {
+    console.log(this.name, this.email, this.phoneNumber, this.birthday);
+  }
 }
+
