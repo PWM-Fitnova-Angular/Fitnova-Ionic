58186462f1d6d5d7a7ae8797ca4182486c6455c9
Raul <raultrejito53@gmail.com>
2025-05-24 15:31:09 +0100

    wip: profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {IonButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon} from "@ionic/angular/standalone";
 import {UserService} from "../../services/user.service";
 import {LocalStorageService} from "../../services/local-storage.service";
 import {RouterLink} from "@angular/router";
+import { addIcons } from 'ionicons';
+import { camera } from 'ionicons/icons';
 
 const userUid : string | null = localStorage.getItem('uid');
-
 
 @Component({
   selector: 'app-profile',
@@ -18,27 +19,50 @@ const userUid : string | null = localStorage.getItem('uid');
     IonContent,
     IonButton,
     IonButtons,
+    IonIcon,
     RouterLink
   ]
 })
 export class ProfileComponent  implements OnInit {
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+
   userImageUrl: any;
   username: any = "";
   email: any = "";
   gender: any = "";
 
-
-  constructor( private userService: UserService, private localStorageService: LocalStorageService) { }
+  constructor(
+    private userService: UserService,
+    private localStorageService: LocalStorageService
+  ) {
+    addIcons({ camera });
+  }
 
   async ngOnInit() {
-
     const data = await this.userService.getUserInfo();
     this.email = data.email;
     this.username= data.nameSurname;
     this.gender = data.gender;
     console.log(userUid)
-    if(userUid){this.userImageUrl = this.localStorageService.getProfileImage(userUid);}
-
+    if(userUid){
+      this.userImageUrl = this.localStorageService.getProfileImage(userUid);
+    }
   }
 
+  openImagePicker() {
+    this.fileInput.nativeElement.click();
+  }
+
+  async onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith('image/') && userUid) {
+      try {
+        const dataUrl = await this.localStorageService.saveProfileImage(userUid, file);
+        this.userImageUrl = dataUrl;
+        console.log('Imagen de perfil actualizada correctamente');
+      } catch (error) {
+        console.error('Error al guardar la imagen:', error);
+      }
+    }
+  }
 }
